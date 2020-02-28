95836aeabc5b344665ab2c5a98bf55af01997af8
Student <example@example.com>
2020-02-28 15:45:04 -0800

    abstracts code further, adds ability for user to choose to order more and to exit program

--- orders.cs
+++ orders.cs
@@ -12,35 +12,62 @@ namespace Orders
         public static void AskOrder ()
         {
             int seconds = 2000;
-            Bread firstBread = new Bread();
-            Pastry firstPastries = new Pastry();
             Console.WriteLine("Welcome to Pierre's Pastry Shop");
             Thread.Sleep(seconds);
             Console.WriteLine("Here We serve Pastries and Bread which would you like to order");
             Thread.Sleep(seconds);
             Console.WriteLine("Press P for Pastry or B for Bread");
             Thread.Sleep(seconds);
+            WhichOrder();
+            Console.WriteLine("Would you like to buy anything else? If you are done with your order type exit");
+            string quitprogram = Console.ReadLine().ToLower();
+            if (quitprogram== "exit")
+            {
+            System.Environment.Exit(0);
+            }
+            else{
+            WhichOrder();
+            }
+
+
+        }
+
+        public static void WhichOrder()
+        {
             string answer = Console.ReadLine().ToLower();
             if (answer == "p")
             {
-                Console.WriteLine("Wonderful, how many pastries are you looking to order");
-                Thread.Sleep(seconds);
-                Console.WriteLine("You should know today were having a sale, 1 for 2$ or 3 for 5$");
-                firstPastries.Quantity = int.Parse(Console.ReadLine());
-                firstPastries.PastriesCost();
-                firstPastries.PrintPastries();
+                PastriesOrder();
             }
             if (answer == "b");
             {
-                Console.WriteLine("Wonderful, how many loafs of bread are you trying to order?");
-                Thread.Sleep(seconds);
-                Console.WriteLine("You should know today were having a sale, buy two get one free");
-                firstBread.OrderedQuantity= int.Parse(Console.ReadLine());
-                firstBread.BreadCosts();
-                firstBread.BreadQuantity();
-                firstBread.PrintBread();
+                BreadsOrder();
             }
-            Console.WriteLine("Would you like to buy anything else?");
+        }
+
+        public static void PastriesOrder()
+        {
+            int seconds = 2000;
+            Pastry firstPastries = new Pastry();
+            Console.WriteLine("Wonderful, how many pastries are you looking to order");
+            Thread.Sleep(seconds);
+            Console.WriteLine("You should know today were having a sale, 1 for 2$ or 3 for 5$");
+            firstPastries.Quantity = int.Parse(Console.ReadLine());
+            firstPastries.PastriesCost();
+            firstPastries.PrintPastries();
+        }
+
+        public static void BreadsOrder()
+        {
+            int seconds = 2000;
+            Bread firstBread = new Bread();
+            Console.WriteLine("Wonderful, how many loafs of bread are you trying to order?");
+            Thread.Sleep(seconds);
+            Console.WriteLine("You should know today were having a sale, buy two get one free");
+            firstBread.OrderedQuantity= int.Parse(Console.ReadLine());
+            firstBread.BreadCosts();
+            firstBread.BreadQuantity();
+            firstBread.PrintBread();
         }
     }
 }
