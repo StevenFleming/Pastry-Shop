f94d413fe6091b6211d7a7ac6a56570bb04fe0b0
Student <example@example.com>
2020-03-02 16:09:41 -0800

    Adds READMe.md

---
+++ README.md
@@ -0,0 +1,47 @@
+# _Doctor Lookup_
+
+#### _Use this app to buy Pastry's and Bread for Pierre's Bakery: 2020_
+
+#### By _**Steven Fleming**_
+
+## Description
+
+_This app allows keeps track of both the quantities and pricing of orders of Pastries and Bread from "Pierre's Bakery"._
+
+## Setup/Installation Requirements
+
+- _Clone the repo to your Desktop_
+- _Install .NET application for Mac or for Windows_
+- _Download or clone this repository to your computer_
+- _Open terminal and move into Project File_
+- _Run "dotnet run" to run the application_
+
+| Spec                                                                      | Input | Output                                                        |
+| :------------------------------------------------------------------------ | :---- | :------------------------------------------------------------ |
+| Displays the cost of a standard pastry                                    | 1     | "Your subtotal is 2\$s"                                       |
+| Displays the cost and amount ordered of a standard pastry                 | 1     | $"You ordered 1 of Pierre's Pastries. Your subtotal is 2$s    |
+|                                                                           |
+| Displays the cost of the "Pastry 3 for 5 deal" and the amount of Pastries | 3     | $"You ordered 3 of Pierre's Pastries. Your subtotal is 5$s    |
+| Displays the cost of a standard Loaf of Bread                             | 1     | "Your subtotal is 5\$s"                                       |
+| Displays the cost and amount ordered of a Loaf of Bread                   | 1     | $"You ordered 1 of Pierre's Bread .Your SubTotal will be 5$s" |
+| Displays the cost for the "Buy two get one free deal" for breads          | 3     | $"You ordered 3 of {Name}.Your SubTotal will be 15$s")        |
+| Displays how many free pieces of bread you got with your order            | 3     | \$Because of our special you got 1 extra loafs of Bread!      |
+
+## Known Bugs
+
+_No known bugs_
+
+## Support and contact details
+
+_12flemings@gmail.com_
+
+## Technologies Used
+
+- .NET
+- C#
+
+### License
+
+_MIT_
+
+Copyright (c) 2020
--- orders.cs
+++ orders.cs
@@ -32,61 +32,13 @@ namespace Orders
             firstBread.BreadQuantity();
             firstBread.PrintBread();
             Thread.Sleep(seconds);
-            Console.WriteLine($" Your total will be {firstBread.TotalCost + firstPastries.TotalCost}$s ");
+            Console.WriteLine($"Your total will be {firstBread.TotalCost + firstPastries.TotalCost}$s ");
             Console.WriteLine("If you are done with your order type exit if you would like to edit your cart enter anything else");
             string quitprogram = Console.ReadLine().ToLower();
             if (quitprogram== "exit")
             {
             System.Environment.Exit(0);
             }
-            // else{
-            // WhichOrder();
-            // }
         }
-
-        // public static void WhichOrder()
-        
-        // {   
-        //     int seconds = 2000;
-        //     Bread firstBread = new Bread();
-        //     Pastry firstPastries = new Pastry();
-        //     Console.WriteLine("Here we serve pastries and let me take your order?");
-        //     Thread.Sleep(seconds);
-        //     PastriesOrder();
-        //     BreadsOrder();
-        //     Console.WriteLine($" You total cost will be {firstPastries.TotalCost + firstBread.TotalCost}");
-        // }
-
-        // public static void PastriesOrder()
-        // {
-        //     Bread firstBread = new Bread();
-        //     Pastry firstPastries = new Pastry();
-        //     int seconds = 2000;
-        //     Console.WriteLine("Were having a sale, 1 for 2$ or 3 for 5$");
-        //     Thread.Sleep(seconds);
-        //     Console.WriteLine("Wonderful, how many pastries are you looking to order");
-        //     firstPastries.Quantity = int.Parse(Console.ReadLine());
-        //     firstPastries.PastriesCost();
-        //     firstPastries.PrintPastries();
-        // }
-
-        // public static void BreadsOrder()
-        // {
-        //     Bread firstBread = new Bread();
-        //     Pastry firstPastries = new Pastry();
-        //     int seconds = 2000;
-        //     Console.WriteLine("You should know today were having a sale, buy two get one free");
-        //     Thread.Sleep(seconds);
-        //     Console.WriteLine("Wonderful, how many loafs of bread do you want to order?");
-        //     firstBread.OrderedQuantity= int.Parse(Console.ReadLine());
-        //     firstBread.BreadCosts();
-        //     firstBread.BreadQuantity();
-        //     firstBread.PrintBread();
-        // }
-
-        // public static string TotalCost(Bread firstBread, Pastry firstPastries)
-        // {
-        //     Console.WriteLine($" Your total is {firstBread.TotalCosts + firstPastries.TotalCost}");
-        // }
     }
 }
